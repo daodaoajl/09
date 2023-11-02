9593d578610bd1f5305c7cf62bba2b259a897aad
daodaoajl <144930715+daodaoajl@users.noreply.github.com>
2023-11-02 16:16:00 +0900

    2-1

--- main.c
+++ main.c
@@ -3,25 +3,20 @@
 
 #define N_STUDENT 5
 
-
 int main(void)
 {
   int i;
   int grade[N_STUDENT];
   
-  /*grade[0] = 10;
-  grade[1] = 20;
-  grade[2] = 30;
-  grade[3] = 40;
-  grade[4] = 50;*/
+  printf("input 5 scores : ");
   
   for(i=0;i<N_STUDENT;i++)
   
-      grade[i] = (i+1)*10;// 위의 주석 처리된 코드를 for문으로  
+      scanf("%d", &grade[i]);
       
   for(i=0;i<N_STUDENT;i++)
    
-      printf("grade[%i] = %i\n" , i, grade[i]);// 출력 
+      printf("grade[%i] = %d\n" , i, grade[i]);// 출력 
   
   system("PAUSE");	
   return 0;
